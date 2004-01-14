8f73ec5d4b2c9fa80f663a4030789dc40dbcdf18
astrand <astrand@8e155d93-173b-4db1-903f-bcee58647ed2>
2004-01-14 17:49:15 +0000

    Indented source using indent-all script

--- cyclades-serial-client/measure.c
+++ cyclades-serial-client/measure.c
@@ -28,67 +28,65 @@
  * Internal Variables
  */
 
-static int	progst;
-static int	progend;
+static int progst;
+static int progend;
 
 void
 start_measure(void)
 {
-	struct tms ts;
+    struct tms ts;
 
-	progst = times(&ts);
+    progst = times(&ts);
 }
+
 void
 cpu_measure(int all)
 {
-	struct tms ts;
-	int usr, sys, tot, pru, prs, prt, msecu, msecs, msect, secs;
+    struct tms ts;
+    int usr, sys, tot, pru, prs, prt, msecu, msecs, msect, secs;
 
-	progend = times(&ts);
+    progend = times(&ts);
 
-	usr = (int) ts.tms_utime;
-	sys = (int) ts.tms_stime;
-	tot = progend - progst;
+    usr = (int) ts.tms_utime;
+    sys = (int) ts.tms_stime;
+    tot = progend - progst;
 
-	pru = usr * 100 / tot;
-	prs = sys * 100 / tot;
-	prt = pru + prs;
+    pru = usr * 100 / tot;
+    prs = sys * 100 / tot;
+    prt = pru + prs;
 
-	msecu = 1000 * usr / HZ;
-	msecs = 1000 * sys / HZ;
-	msect = msecu + msecs;
+    msecu = 1000 * usr / HZ;
+    msecs = 1000 * sys / HZ;
+    msect = msecu + msecs;
 
-	printf ("%s: \n\
-	%5d ms (%2d%%) user, %5d ms (%2d%%) sys, %5d ms (%3d%%) total\n",
-		Idmsg, msecu, pru, msecs, prs, msect, prt);
+    printf("%s: \n\
+	%5d ms (%2d%%) user, %5d ms (%2d%%) sys, %5d ms (%3d%%) total\n", Idmsg, msecu, pru, msecs, prs, msect, prt);
 
-	if (all) {
-		secs = tot / HZ;
-		if (secs == 0) secs = 1;
-		if (devreads == 0) devreads = 1;
-		if (devwrites == 0) devwrites = 1;
-		if (netreads == 0) netreads = 1;
-		if (netwrites == 0) netwrites = 1;
+    if (all) {
+	secs = tot / HZ;
+	if (secs == 0)
+	    secs = 1;
+	if (devreads == 0)
+	    devreads = 1;
+	if (devwrites == 0)
+	    devwrites = 1;
+	if (netreads == 0)
+	    netreads = 1;
+	if (netwrites == 0)
+	    netwrites = 1;
 
-		printf ( "%s:\n\
+	printf("%s:\n\
 	devrds : %6d (%6d null) , %3d  reads/sec, %.2f bytes/read)\n\
-	devwrs : %6d (%6d null) , %3d writes/sec, %.2f bytes/write)\n",
-	Idmsg,
-	devreads, devnreads, devreads/secs, (double) devrbytes/devreads,
-	devwrites, devnwrites, devwrites/secs, (double) devwbytes/devwrites);
+	devwrs : %6d (%6d null) , %3d writes/sec, %.2f bytes/write)\n", Idmsg, devreads, devnreads, devreads / secs, (double) devrbytes / devreads, devwrites, devnwrites, devwrites / secs, (double) devwbytes / devwrites);
 
-		printf ( "%s:\n\
+	printf("%s:\n\
 	netrds : %6d (%6d null) , %3d  reads/sec, %.2f bytes/read)\n\
-	netwrs : %6d (%6d null) , %3d writes/sec, %.2f bytes/write)\n",
-	Idmsg,
-	netreads, netnreads, netreads/secs, (double) netrbytes/netreads,
-	netwrites, netnwrites, netwrites/secs, (double) netwbytes/netwrites);
+	netwrs : %6d (%6d null) , %3d writes/sec, %.2f bytes/write)\n", Idmsg, netreads, netnreads, netreads / secs, (double) netrbytes / netreads, netwrites, netnwrites, netwrites / secs, (double) netwbytes / netwrites);
 
-		printf ( "%s:\n\
-	scheds : %6d (%3d scheds/sec)\n",Idmsg,
-	ioscheds, ioscheds/secs);
+	printf("%s:\n\
+	scheds : %6d (%3d scheds/sec)\n", Idmsg, ioscheds, ioscheds / secs);
 
-	}
+    }
 }
 
 # endif
